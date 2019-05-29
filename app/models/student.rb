136a41019e80512c1f477c861ae84b18b167a442
Joshua Tan <jtsarvida@gmail.com>
2019-05-29 07:30:55 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,13 +3,4 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
   
-  def active?
-    if self.active == true 
-      print "active"
-    elsif self.active == false 
-      print "inactive"
-    else
-      print "whoops"
-    end
-  end
 end
